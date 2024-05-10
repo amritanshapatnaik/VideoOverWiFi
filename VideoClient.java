ae0ebdab8eb83616efb4ca4b1cf703104371ce68
amritanshapatnaik <119752904+amritanshapatnaik@users.noreply.github.com>
2024-05-10 22:16:14 +0530

    Add files via upload

--- VideoClient.java
+++ VideoClient.java
@@ -8,25 +8,21 @@ import java.net.*;
 public class VideoClient {
     static Socket socket;
 
-public static void main(String[] args) {
-try {
-socket = new Socket("localhost", 2323);
-while (true) {
-InputStream inStream =
-socket.getInputStream();
-DataInputStream dataInputStream = new
-DataInputStream(inStream);
-int length = dataInputStream.readInt();
-byte[] buffer = new byte[length];
-12
-13
-dataInputStream.readFully(buffer, 0, buffer.length);
-Mat frame = Imgcodecs.imdecode(new MatOfByte(buffer), Imgcodecs.IMREAD_UNCHANGED);
-HighGui.imshow("Frame", frame);
-HighGui.waitKey(1);
-}
-} catch (IOException e) {
-e.printStackTrace();
-}
-}
+    public static void main(String[] args) {
+        try {
+            socket = new Socket("localhost", 2323);
+            while (true) {
+                InputStream inStream = socket.getInputStream();
+                DataInputStream dataInputStream = new DataInputStream(inStream);
+                int length = dataInputStream.readInt();
+                byte[] buffer = new byte[length];
+                dataInputStream.readFully(buffer, 0, buffer.length);
+                Mat frame = Imgcodecs.imdecode(new MatOfByte(buffer), Imgcodecs.IMREAD_UNCHANGED);
+                HighGui.imshow("Frame", frame);
+                HighGui.waitKey(1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- VideoServer.java
+++ VideoServer.java
@@ -8,46 +8,38 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class VideoServer {
-static Socket socket;
-static VideoCapture camera;
-public static void main(String[] args) {
-System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-try {
-ServerSocket serverSocket = new
-ServerSocket(2323);
-socket = serverSocket.accept();
-camera = new VideoCapture(0);
-if (!camera.isOpened()) {
-System.out.println("Error! Camera
-can’t be opened!");
-return;
-}
-10
-11
-else {
-System.out.println("Camera Found!");
-}
-Mat frame = new Mat();
-while (true) {
-if (camera.read(frame)) {
-MatOfByte outputStream = new
-MatOfByte();
-Imgcodecs.imencode(".png", frame,
-outputStream);
-byte[] buffer = outputStream.toArray();
-OutputStream outStream =
-socket.getOutputStream();
-DataOutputStream dataOutputStream = new
-DataOutputStream(outStream);
-dataOutputStream.writeInt
-(buffer.length);
-dataOutputStream.write(buffer, 0,
-buffer.length);
-}
-}
-} catch (IOException e) {
-e.printStackTrace();
-}
-}
+    static Socket socket;
+    static VideoCapture camera;
+
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        try {
+            ServerSocket serverSocket = new ServerSocket(2323);
+            socket = serverSocket.accept();
+            camera = new VideoCapture(0);
+            if (!camera.isOpened()) {
+                System.out.println("Error! Cameracan’t be opened!");
+                return;
+            } else {
+                System.out.println("Camera Found!");
+            }
+            Mat frame = new Mat();
+            while (true) {
+                if (camera.read(frame)) {
+                    MatOfByte outputStream = new MatOfByte();
+                    Imgcodecs.imencode(".png", frame, outputStream);
+                    byte[] buffer = outputStream.toArray();
+                    OutputStream outStream = socket.getOutputStream();
+                    DataOutputStream dataOutputStream = new DataOutputStream(outStream);
+                    dataOutputStream.writeInt(buffer.length);
+                    dataOutputStream.write(buffer, 0,
+                            buffer.length);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
